fd3538da670e1ff4683813ca4a8a857bbabd4e7d
rashmisajjan <rashmisajjan@localhost>
2015-12-03 11:09:30 +0530

    faq issue fixed

---
+++ ummapp/umm_tool/umm_app/admin.py
@@ -0,0 +1,117 @@
+from django.contrib import admin
+from django.db import models
+from django import forms
+from .models import Quarter, Category, Task, AdditionData, ColumnData, ComboUpdate, BudgetBand, Goal, Question, GoalTaskMap, ExtraTask
+
+
+class ComboInline(admin.TabularInline):
+    model = ComboUpdate
+    extra = 0
+
+
+class QuarterAdmin(admin.ModelAdmin):
+    inlines = [ComboInline, ]
+    list_display = (u'quarter', 'quarter_year', 'created_date', 'modified_date')
+
+    class Meta:
+        model = Quarter
+
+
+class BudgetBandAdmin(admin.ModelAdmin):
+    list_display = ('parent_quarter_id', 'created_date', 'modified_date')
+    formfield_overrides = {models.TextField: {'widget': forms.Textarea(attrs={'class':'ckeditor'})}, }
+
+    class Media:
+        js = ('ckeditor/ckeditor.js',)
+
+    class Meta:
+        model = BudgetBand
+
+
+class ComboUpdateAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_date', 'modified_date')
+    formfield_overrides = {models.TextField: {'widget': forms.Textarea(attrs={'class':'ckeditor'})}, }
+
+    class Media:
+        js = ('ckeditor/ckeditor.js',)
+
+    class Meta:
+        model = ComboUpdate
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name', 'parent_quarter_id', 'created_date')
+
+    class Meta:
+        model = Category
+
+
+class TaskAdmin(admin.ModelAdmin):
+    # inlines = [ColumnDataInline, AdditionDataInline]
+    list_display = ('task_name', 'parent_category_id', 'created_date')
+
+    class Meta:
+        model = Task
+
+
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('goal_name', 'created_date')
+
+    class Meta:
+        model = Goal
+
+
+class ExtraTaskAdmin(admin.ModelAdmin):
+    list_display = ('extra_task_name', 'parent_goal_id')
+
+    class Meta:
+        model = ExtraTask
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'parent_goal_id')
+
+    class Meta:
+        model = Question
+
+
+class GoalTaskMapAdmin(admin.ModelAdmin):
+    list_display = ('parent_goal_id', 'get_task', 'created_date')
+
+    class Meta:
+        model = GoalTaskMap
+
+
+class ColumnDataAdmin(admin.ModelAdmin):
+    list_display = ('column_name', 'parent_task_id', 'column_value')
+    formfield_overrides = {models.TextField: {'widget': forms.Textarea(attrs={'class':'ckeditor'})}, }
+
+    class Media:
+        js = ('ckeditor/ckeditor.js',)
+
+    class Meta:
+        model = ColumnData
+
+
+class AdditionDataAdmin(admin.ModelAdmin):
+    list_display = ('email_pitch_guidelines', 'implementation_guide', 'faq', 'elevator_pitch')
+    formfield_overrides = {models.TextField: {'widget': forms.Textarea(attrs={'class':'ckeditor'})}, }
+
+    class Media:
+        js = ('ckeditor/ckeditor.js',)
+
+    class Meta:
+        model = AdditionData
+
+
+admin.site.register(Quarter, QuarterAdmin)
+admin.site.register(BudgetBand, BudgetBandAdmin)
+admin.site.register(ComboUpdate, ComboUpdateAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Goal, GoalAdmin)
+admin.site.register(ExtraTask, ExtraTaskAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(GoalTaskMap, GoalTaskMapAdmin)
+admin.site.register(ColumnData, ColumnDataAdmin)
+admin.site.register(AdditionData, AdditionDataAdmin)
---
+++ ummapp/umm_tool/umm_app/models.py
@@ -0,0 +1,132 @@
+from django.db import models
+
+
+class Quarter(models.Model):
+    QUARTER_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+    )
+    quarter = models.IntegerField(choices=QUARTER_CHOICES, default=1)
+    quarter_year = models.IntegerField()
+    created_date = models.DateTimeField()
+    modified_date = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ('quarter', 'quarter_year',)
+
+    def __unicode__(self):
+            return u"%s-%d" % (self.quarter, self.quarter_year)
+
+
+class BudgetBand(models.Model):
+    parent_quarter_id = models.ForeignKey(Quarter)
+    band_details = models.TextField()
+    created_date = models.DateTimeField()
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return "%s" % (self.band_details)
+
+
+class ComboUpdate(models.Model):
+    parent_quarter_id = models.ForeignKey(Quarter)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_date = models.DateTimeField()
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return "%s %s" % (self.title, self.description)
+
+
+class Category(models.Model):
+    parent_quarter_id = models.ForeignKey(Quarter)
+    category_name = models.CharField(max_length=250, unique=True)
+    created_date = models.DateTimeField()
+    modified_date = models.DateTimeField(auto_now=True)
+    is_disable = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('category_name', 'parent_quarter_id',)
+
+    def __unicode__(self):
+        return "%s-%s" % (self.category_name, self.parent_quarter_id)
+
+
+class Task(models.Model):
+    parent_category_id = models.ForeignKey(Category)
+    task_name = models.CharField(max_length=250)
+    created_date = models.DateField()
+    modified_date = models.DateTimeField(auto_now=True)
+    is_disable = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('task_name', 'parent_category_id',)
+
+    def __unicode__(self):
+        return "%s|%s" % (self.task_name, self.parent_category_id)
+
+
+class Goal(models.Model):
+    goal_name = models.CharField(max_length=250)
+    created_date = models.DateField()
+    modified_date = models.DateTimeField(auto_now=True)
+    is_disable = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return "%s" % (self.goal_name)
+
+
+class Question(models.Model):
+    parent_goal_id = models.ForeignKey(Goal)
+    question = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return "%s" % (self.question)
+
+
+class ExtraTask(models.Model):
+    parent_goal_id = models.ForeignKey(Goal)
+    extra_task_name = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return "%s" % (self.extra_task_name)
+
+
+class GoalTaskMap(models.Model):
+    parent_task_id = models.ManyToManyField(Task)
+    parent_goal_id = models.ForeignKey(Goal)
+    created_date = models.DateField()
+    modified_date = models.DateTimeField(auto_now=True)
+    is_disable = models.BooleanField(default=False)
+
+    def get_task(self):
+        return "\n".join(['%s-%s' % (q.task_name, q.parent_category_id) for q in self.parent_task_id.all()])
+
+    def __unicode__(self):
+        return "%s %s" % (self.parent_task_id, self.parent_goal_id)
+
+
+class ColumnData(models.Model):
+    parent_task_id = models.ForeignKey(Task)
+    column_name = models.CharField(max_length=250)
+    column_value = models.TextField()
+    is_disable = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return "%s %s %s" % (self.column_name, self.column_name, self.column_value)
+
+
+class AdditionData(models.Model):
+    parent_task_id = models.ForeignKey(Task)
+    email_pitch_guidelines = models.TextField()
+    implementation_guide = models.TextField()
+    faq = models.TextField()
+    elevator_pitch = models.TextField()
+    is_disable = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return "%s %s %s %s" % (self.email_pitch_guidelines, self.implementation_guide,self.faq,self.elevator_pitch)
