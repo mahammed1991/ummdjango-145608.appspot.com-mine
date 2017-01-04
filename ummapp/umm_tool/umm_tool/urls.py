ee38ea78cbfb78aa252fc6b5295f1d833b442da5
sheetalsharma <sheetalsharma@localhost>
2017-01-04 16:55:01 +0530

    updated code for process creation

--- ummapp/umm_tool/umm_tool/urls.py
+++ ummapp/umm_tool/umm_tool/urls.py
@@ -33,25 +33,40 @@ urlpatterns = [
     url(r'^combodata/$', 'umm_app.views.combo_data', name='combo_data'),
     url(r'^budget_band/$', 'umm_app.views.budget_band', name='budget_band'),
     url(r'^_ah/', include('djangae.urls')),
-    url(r'^apollo/$', "umm_app.views.apollo_home", name="apollo_home"),
-    url(r'^umm/manage-admin/','umm_app.views.manage_admin', name='manage_admin'),
-    url(r'^umm/create-process/','umm_app.views.create_process', name='create_process'),
-    url(r'^umm/view-process/','umm_app.views.view_process', name='view_process'),
-    url(r'^umm/update-process/(?P<pk>[0-9]+)$','umm_app.views.update_process', name='update_process'),
-    url(r'^umm/create-program-type/','umm_app.views.create_program_type', name='create_program_type'),
-    url(r'^umm/view-program-type/','umm_app.views.view_program_type', name='view_program_type'),
-    url(r'^umm/update-program-type/(?P<pk>[0-9]+)$','umm_app.views.update_program_type', name='update_program_type'),
-    url(r'^umm/create-program-task/','umm_app.views.create_program_task', name='create_program_task'),
-    url(r'^umm/view-program-task/','umm_app.views.view_program_task', name='view_program_task'),
-    url(r'^umm/update-program-task/(?P<pk>[0-9]+)$','umm_app.views.update_program_task', name='update_program_task'),
-    #url(r'^umm/create-task-data/','umm_app.views.create_task_data', name='create_task_data'),
+    #url(r'^apollo/$', "umm_app.views.apollo_home", name="apollo_home"),
+    url(r'^knowledgebase/admin/','umm_app.views.manage_admin', name='manage_admin'),
+    #url(r'^umm/create-process/','umm_app.views.create_process', name='create_process'),
+    #url(r'^umm/view-process/','umm_app.views.view_process', name='view_process'),
+    #url(r'^umm/update-process/(?P<pk>[0-9]+)$','umm_app.views.update_process', name='update_process'),
+    #url(r'^umm/create-program-type/','umm_app.views.create_program_type', name='create_program_type'),
+    #url(r'^umm/view-program-type/','umm_app.views.view_program_type', name='view_program_type'),
+    #url(r'^umm/update-program-type/(?P<pk>[0-9]+)$','umm_app.views.update_program_type', name='update_program_type'),
+    #url(r'^umm/create-program-task/','umm_app.views.create_program_task', name='create_program_task'),
+    #url(r'^umm/view-program-task/$','umm_app.views.view_program_task', name='view_program_task'),
+    #url(r'^umm/update-program-task/(?P<pk>[0-9]+)$','umm_app.views.update_program_task', name='update_program_task'),
+    #url(r'^umm/create-task-data/$','umm_app.views.create_task_data', name='create_task_data'),
 
-    url(r'^create/','umm_app.views.create', name='create'),
-    url(r'^get-process/','umm_app.views.get_process', name='get_process'),
-    url(r'^process/(?P<process_id>[0-9]+)/(?P<sub_process>[A-Za-z0-9-]+)/','umm_app.views.create_task_data',name='create_task_data'),
+    
     url(r'^get-program-tasks/(?P<program_type_id>[0-9]+)/','umm_app.views.get_program_tasks',name='get_program_tasks'),
     url(r'^get-column-name/','umm_app.views.get_column_name',name='get_column_name'),
     url(r'^add-task-data/','umm_app.views.add_task_data',name='add_task_data'),
     url(r'^get-task-data/(?P<task_id>[0-9]+)','umm_app.views.get_task_data',name='get_task_data'),
-    url(r'^process/(?P<process_name>[A-Za-z0-9-]+)/(?P<process_id>[0-9]+)/$','umm_app.views.show_process_data',name='show_process_data'),
+    
+
+
+
+    # API
+
+    # Get all processes
+    url(r'^get-process/$','umm_app.views.get_process', name='get_process'),
+    # Get All Subprocesses
+    url(r'^process/(?P<reference_id>[0-9]+)/subprocesses$', 'umm_app.views.subprocess_handler'),
+    
+
+    #Views
+
+    url(r'^process/(?P<reference_id>[0-9]+)/show$', 'umm_app.views.subprocess_handler', name='subprocess_handler'),
+    url(r'^process/(?P<process_id>[0-9]+)/sp/(?P<sprocess_name>[A-Za-z0-9-]+)/edit$','umm_app.views.create_task_data',name=''),
+    url(r'^process/create/$','umm_app.views.process_handler', name='process_handler'),
+    url(r'^sp/(?P<sprocess_name>[A-Za-z0-9-]+)/$','umm_app.views.show_process_data', name='show_process_data'),
 ]
